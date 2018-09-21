9ca12339400d9500347c33c2576e6f3b249bc6f7
vanzi <ivanl487@gmail.com>
2018-09-21 17:11:45 +0300

    try/catch added

--- src/proxy.js
+++ src/proxy.js
@@ -2,13 +2,13 @@ module.exports = {
     start: function () {
         // Config
         var config = require('./config');
-        
+
         // Config check
         if (!config.proxyPort || !config.elasticUrl) {
             console.log('Fill proxy port number, elasticsearch url beafore start');
             return;
         }
-        
+
         // Imports
         var http = require('http'),
             httpProxy = require('http-proxy'),
@@ -30,7 +30,7 @@ module.exports = {
                         console.error(e.message);
                     }
                 }).on('error', (e) => {
-                    console.error(`Got error: ${e.message}`);
+                    console.error(e.message);
                 });
 
                 proxy.web(req, res, config.proxyOptions);
@@ -53,79 +53,83 @@ module.exports = {
                 body = Buffer.concat([body, data]);
             });
             proxyRes.on('end', function () {
-                // Elastic join
-                if (req.extraQuery && req.extraQuery.query) {
-                    // Main query response and new response
-                    var mainQuery = JSON.parse(body.toString()),
-                        readyResponse = null;
+                try {
+                    // Elastic join
+                    if (req.extraQuery && req.extraQuery.query) {
+                        // Main query response and new response
+                        var mainQuery = JSON.parse(body.toString()),
+                            readyResponse = null;
 
-                    // Pagination vars
-                    var from = req.extraQuery.from ? req.extraQuery.from : 0;
-                        size = req.extraQuery.size + from,
-                        realSize = req.extraQuery.size;
+                        // Pagination vars
+                        var from = req.extraQuery.from ? req.extraQuery.from : 0;
+                            size = req.extraQuery.size + from,
+                            realSize = req.extraQuery.size;
 
-                    // Override pagination
-                    var queryObj = req.extraQuery;
-                    delete req.extraQuery;
-                    delete queryObj.from;
-                    queryObj.size = size;
+                        // Override pagination
+                        var queryObj = req.extraQuery;
+                        delete req.extraQuery;
+                        delete queryObj.from;
+                        queryObj.size = size;
 
-                    // Extra queries
-                    for (var element of queryObj.query.bool.must.simple_query_string.fields) {
-                        var xhr = new XMLHttpRequest();
-                        // Wait for response
-                        xhr.open('POST', config.elasticUrl, false);
-                        xhr.onreadystatechange = function() {
-                            if (xhr.readyState == 4 && xhr.status == 200) {
-                                // Make new response body
-                                var response = JSON.parse(xhr.responseText);
-                                if (readyResponse === null) {
-                                    readyResponse = response;
-                                } else {
-                                    readyResponse.hits.hits = readyResponse.hits.hits.concat(response.hits.hits);
+                        // Extra queries
+                        for (var element of queryObj.query.bool.must.simple_query_string.fields) {
+                            var xhr = new XMLHttpRequest();
+                            // Wait for response
+                            xhr.open('POST', config.elasticUrl, false);
+                            xhr.onreadystatechange = function() {
+                                if (xhr.readyState == 4 && xhr.status == 200) {
+                                    // Make new response body
+                                    var response = JSON.parse(xhr.responseText);
+                                    if (readyResponse === null) {
+                                        readyResponse = response;
+                                    } else {
+                                        readyResponse.hits.hits = readyResponse.hits.hits.concat(response.hits.hits);
+                                    }
                                 }
                             }
-                        }
-                        // Additional queries logic
-                        if (readyResponse !== null) {
-                            // Set exclusions by elastic _id
-                            var excludeById = [];
-                            var len = Object.keys(readyResponse.hits.hits).length;
-                            for (var i = 0; i < len; i++) {
-                                excludeById.push(readyResponse.hits.hits[i]._id);
+                            // Additional queries logic
+                            if (readyResponse !== null) {
+                                // Set exclusions by elastic _id
+                                var excludeById = [];
+                                var len = Object.keys(readyResponse.hits.hits).length;
+                                for (var i = 0; i < len; i++) {
+                                    excludeById.push(readyResponse.hits.hits[i]._id);
+                                }
+                                queryObj.query.bool.must_not.terms._id = excludeById;
                             }
-                            queryObj.query.bool.must_not.terms._id = excludeById;
+                            // Choose query fields
+                            queryObj.query.bool.must.simple_query_string.fields = [element];
+                            xhr.send(JSON.stringify(queryObj));
+                        };
+                        // Pagination
+                        var len = Object.keys(readyResponse.hits.hits).length,
+                            out = [],
+                            j = 0;
+                        for (var i = from; i < size; i++) {
+                            if (!readyResponse.hits.hits[i]) {
+                                break;
+                            }
+                            out[j++] = readyResponse.hits.hits[i];
                         }
-                        // Choose query fields
-                        queryObj.query.bool.must.simple_query_string.fields = [element];
-                        xhr.send(JSON.stringify(queryObj));
-                    };
-                    // Pagination 
-                    var len = Object.keys(readyResponse.hits.hits).length,
-                        out = [],
-                        j = 0;
-                    for (var i = from; i < size; i++) {
-                        if (!readyResponse.hits.hits[i]) {
-                            break;
-                        }
-                        out[j++] = readyResponse.hits.hits[i];
+                        readyResponse.hits.hits = out;
+                        out = null;
+                        // Aggregations
+                        readyResponse.aggregations = mainQuery.aggregations;
+                        // Total hits
+                        readyResponse.hits.total = mainQuery.hits.total;
+                        // Form response
+                        var resString = JSON.stringify(readyResponse);
+                        const t = Buffer.from(resString);
+                        proxyRes.headers['content-length'] = t.length;
+                        res.writeHead(200, proxyRes.headers);
+                        res.end(t.toString());
                     }
-                    readyResponse.hits.hits = out;
-                    out = null;
-                    // Aggregations
-                    readyResponse.aggregations = mainQuery.aggregations;
-                    // Total hits
-                    readyResponse.hits.total = mainQuery.hits.total;
-                    // Form response
-                    var resString = JSON.stringify(readyResponse);
-                    const t = Buffer.from(resString);
-                    proxyRes.headers['content-length'] = t.length;
+                    // No query response
                     res.writeHead(200, proxyRes.headers);
-                    res.end(t.toString());
+                    res.end(body.toString());
+                } catch (e) {
+                    console.error(e.message);
                 }
-                // No query response
-                res.writeHead(200, proxyRes.headers);
-                res.end(body.toString());
             });
         });
 
